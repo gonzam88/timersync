e743d9364307dc0f1bb55c2cf1d81f8492c73ffb
gonza <gonzamoiguer@gmail.com.com>
2019-12-06 14:46:10 -0300

    fix nosleep y movido cosas de lugar

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/client/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static('public'))
 
 var port = process.env.PORT || 3000;
 
